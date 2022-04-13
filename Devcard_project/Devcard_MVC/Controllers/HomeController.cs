8b945bb66487542160b42259c2d86e86f160f111
erfan <erfan@DESKTOP-A1OQCOV>
2022-04-13 10:29:38 +0430

    asset folder added to project

--- Devcard_project/Devcard_MVC/Controllers/HomeController.cs
+++ Devcard_project/Devcard_MVC/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Devcard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+      public IActionResult Contact()
         {
             return View();
         }
